ba7c4a5cf70abaac811b806c3b966ecce1af766c
Luca Potenza <potenzaluca32@gmail.com>
2024-06-26 17:45:10 +0200

    added es cerca elementi

---
+++ Cercareelementi/dom.js
@@ -0,0 +1,10 @@
+// Cerca tutti gli elementi con classe "paragrafo".
+const myP = document.getElementsByClassName("paragrafo");
+console.log(myP);
+
+// Cerca l' elemento con id "elemento-3"
+const myLi = document.getElementById("elemento-3");
+console.log(myLi);
+// Cerca tutti gli elementi h2 della pagina
+const myTitle = document.querySelectorAll("h2");
+console.log(myTitle);
